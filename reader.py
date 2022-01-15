0ad8000406780b1cf9ad9787633dba0559a59ac8
MirekRogowski <93225975+MirekRogowski@users.noreply.github.com>
2022-01-15 18:54:45 +0100

    Update reader.py
    dunkcrion get_class_writer

--- reader.py
+++ reader.py
@@ -11,10 +11,8 @@ class AbstractReader:
     ALLOWED_EXTENSIONS = ("json", "csv", "pickle")
 
     def __init__(self, filename, filepath=f"{os.getcwd()}"):
-        print("create object")
         self.filename = filename
         self.filepath = filepath
-        # self.filepath_new =
         self.file_data = []
         self.file_type = self.check_filetype()
         self.validate = self.validate()
@@ -32,11 +30,8 @@ class AbstractReader:
             return os.path.join(self.filepath, self.filename)
         return self.filename
 
-    def check_file(self):
-        self.open_file() if os.path.exists(self.filename) else self.list_directory()
-
     def list_directory(self):
-        print(f"Brak pliku: {self.filename} w katalogu: {self.filepath}")
+        # print(f"Brak pliku: {self.filename} w katalogu: {self.filepath}")
         for line in os.listdir(self.filepath):
             print(f"{line:55} file") if os.path.isfile(line) else print(f"{line:55} <DIR>")
         return exit()
@@ -44,106 +39,129 @@ class AbstractReader:
     def new_values(self, new_value):
         for lista in new_value:
             item = lista.split(",")
-            print(item)
             y = int(item[0].strip())
             x = int(item[1].strip())
             value = item[2].strip()
-            print(value)
             self.file_data[y][x] = value
 
     def print_data(self):
-        print(self.filename)
-        print(self.filepath)
+        # print(self.filename)
+        # print(self.filepath)
         print(self.file_data)
-        print(self.file_type)
-        print(self.validate)
+        print(type(self.file_data))
+        # print(self.file_type)
+        # print(self.validate)
 
 
-class FileCsv(AbstractReader):
-    # def __init__(self, filename, filepath=""):
-    #     super.__init__(filename, filepath)
-        # print(super.__init__(filename, filepath))
+class FileCsvReader(AbstractReader):
+    def check_file(self):
+        self.read_file() if os.path.exists(self.filename) else self.list_directory()
 
-    def open_file(self):
-        with open(self.filename, newline="\n") as f:
+        # open_file(os.path.join(directory, file)) if os.path.isfile(file) else list_directory(file, directory)
+
+    def read_file(self):
+        with open(os.path.join(self.filepath, self.filename), newline="\n") as f:
+        # with open(self.filename, newline="\n") as f:
             for line in csv.reader(f):
                 self.file_data.append(line)
 
+
+class FileJsonReader(AbstractReader):
+    def read_file(self):
+        with open(self.filename) as f:
+            temp_data = json.load(f)
+            self.file_data = json.loads(temp_data)
+
+
+class FilePickleReader(AbstractReader):
+    def read_file(self):
+        with open(self.filename, "rb") as f:
+            self.file_data = pickle.load(f)
+
+
+class FileCsvWriter:
+    def __init__(self, filename, file_date):
+        self.filename = filename
+        self.file_date = file_date
+
     def write_file(self):
-        t = input("\nZapisac dane do pliku: (T/N)? ")
-        if t.lower() == "t":
-            with open("test.csv", "w", newline="") as f:
-                csv_writer = csv.writer(f)
-                for line in self.file_data:
-                    csv_writer.writerow(line)
-            print(f"\nDane zapisane w pliku: ")
-        else:
-            print("\nNie zapisano zmian")
-            exit()
+        with open(self.filename, "w", newline="") as f:
+            csv_writer = csv.writer(f)
+            for line in self.file_date:
+                csv_writer.writerow(line)
 
 
-class FileJson(AbstractReader):
+class FileJsonWriter:
+    def __init__(self, filename, file_date):
+        self.filename = filename
+        self.file_date = file_date
 
     def write_file(self):
-        json_string = json.dumps(open_csv.file_data) #read from object open_csv
+        json_string = json.dumps(self.file_date) #read from object open_csv
         with open(self.filename, "w") as f:
             json.dump(json_string, f)
 
-    def read_file(self):
-        # print("funkcja read_from_json_file ")
-        with open(self.filename) as f:
-            self.file_data = json.load(f)
+
+class FilePickleWriter:
+    def __init__(self, filename, file_date):
+        self.filename = filename
+        self.file_date = file_date
+
+    def write_file(self):
+        with open(self.filename, "wb") as f:
+            pickle.dump(self.file_date, f)
 
 
-        # response = requests.request("GET", url, headers=headers, params=querystring)
-        # out_url = response.json()
-        # with open("out.json", 'w') as json_file:
-        #     json.dump(out_url, json_file)
+def get_class_reader(file_name, file_path):
+    suffix = pathlib.Path(file_name).suffix[1:]
+    if suffix == "csv":
+        return FileCsvReader(file_name, file_path)
+    if suffix == "json":
+        return FileJsonReader(file_name, file_path)
+    if suffix == "pickle":
+        return FilePickleReader(file_name, file_path)
 
 
+def get_class_writer(file_name, file_date):
+    suffix = pathlib.Path(file_name).suffix[1:]
+    if suffix == "csv":
+        return FileCsvWriter(file_name, file_date)
+    if suffix == "json":
+        return FileJsonWriter(file_name, file_date)
+    if suffix == "pickle":
+        return FilePickleWriter(file_name, file_date)
+
+def print_test():
+    print(sys.argv[1])
+    print(sys.argv[2])
+    print("_" * 50)
+    print(pathlib.Path(sys.argv[1]).name)
+    print("_" * 50)
+    print(pathlib.Path(sys.argv[1]).is_absolute())
+    print("_" * 50)
+    print(pathlib.Path(sys.argv[1]).joinpath())
+    print("_" * 50)
+    print(pathlib.Path(sys.argv[1]))
+    print("_" * 50)
+    print(sys.argv[1].split(os.sep)[-1])
+    print("_" * 50)
+    print(os.path.dirname(sys.argv[1]))
 
 
-        # t = input("\nZapisac dane do pliku: (T/N)? ")
-        # json_object = json.dump(self.file_data)
-        # if t.lower() == "t":
-        #     with open(self.filename, "w") as f:
-        #         f.write(json_object)
-        #     print(f"\nDane zapisane w pliku: ")
-        # else:
-        #     print("\nNie zapisano zmian")
-        #     exit()
+file_name_reader = pathlib.Path(sys.argv[1]).name
+file_path_reader = os.path.dirname(sys.argv[1])
+file_name_writer = pathlib.Path(sys.argv[2]).name
+file_path_writer = os.path.dirname(sys.argv[2])
 
+fr = get_class_reader(file_name_reader, file_path_reader)
+print(fr.filename, fr.filepath)
+fr.check_file()
+fr.print_data()
 
-    # with open("out.json", 'w') as json_file:
-    #     json.dump(out_url, json_file)
-    #
+# data = json.loads(fr.file_data)
+# data1 = list(data)
+# print(data, type(data))
+# print(data1, type(data1) )
 
-
-    # def read_from_csv_file(self):
-    #     with open("data.csv", newline='') as f:
-    #         reader = csv.reader(f)
-    #         for i in reader:
-    #             self.file_data.append([i[0], i[1]])
-
-
-# abstract = AbstractReader("data.txt")
-# print(abstract.file_type)
-# print(abstract.validate)
-# file = pathlib.Path("c:\tool\data.csv")
-# print(file.suffix)
-# print(file.name)
-# print(file.parts)
-open_csv = FileCsv("data.csv")
-open_csv.check_file()
-# open_csv.print_data()
-print(sys.argv[1:])
-open_csv.new_values(sys.argv[1:])
-open_csv.print_data()
-open_csv.write_file()
-
-open_json = FileJson("data.json")
-open_json.write_file()
-open_json.print_data()
-print("reda file")
-open_json.read_file()
-open_json.print_data()
+fw = get_class_writer(file_name_writer, fr.file_data)
+fw.write_file()
